fa27b8f243d4015a099fab5b91b6a0fc641a8819
Altin Zhuta <altin.zhuta@deutschebahn.com>
2022-11-17 12:19:23 +0100

    init

---
+++ skript.js
@@ -0,0 +1,61 @@
+import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
+
+var currentPopup = undefined;
+var isCoWebSiteOpened =  false;
+var urlTutorial = "https://db-planet.deutschebahn.com/pages/telefonie/apps/content/workadventure-erste-schritte";
+//var urlFeedback = "mailto:DB.Systel.CommunicationServices.EVS@deutschebahn.com";
+var urlFeedback = "https://forms.office.com/Pages/ResponsePage.aspx?id=nC2noeZJbU-a9lqvoRg7_f26WHDvlOFNi_8Y43fECOdUMDVDTUpUUDRONkxHMzdLQ09WRlQxUUZSMS4u";
+var urlInfo = "https://db.de/workadventure";
+var urlInfoOrder = "https://dbserviceportal.service-now.com/serviceportal?id=sc_cat_item&category_sys_id=undefined&sys_id=0fa1b33e1b4bf010159842229b4bcb0e";
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
+
+var zoneFeedback = "feedback";
+var zoneTutorial = "tutorial";
+
+WA.room.onEnterZone(zoneTutorial, () => {
+   currentPopup =  WA.ui.openPopup("popUpTutorial","Tutorial ansehen?",[
+        {
+            label: "OK",
+            callback: (popup => {
+                WA.nav.openTab(urlTutorial);
+                isCoWebSiteOpened = true;
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onLeaveZone(zoneTutorial, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
+
+WA.room.onEnterZone(zoneFeedback, () => {
+   currentPopup =  WA.ui.openPopup("popUpFeedback","Hier kannst du Feedback abgeben.",[
+        {
+            label: "Feedback",
+            callback: (popup => {
+                WA.nav.openCoWebSite(urlFeedback);
+                isCoWebSiteOpened = true;
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onLeaveZone(zoneFeedback, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
